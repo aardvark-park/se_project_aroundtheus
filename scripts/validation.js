29a1e360c478f1d243955a7f01cea81946314577
Brandon Kincaid <kincaid.brandon@live.com>
2023-11-18 20:04:45 -0800

    add validation for inputs - incomplete

---
+++ scripts/validation.js
@@ -0,0 +1,41 @@
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", () => console.log("input ran"));
+  });
+}
+
+function enableValidation(options) {
+  const formEls = [...document.querySelectorAll(options.formSelector)];
+  formEls.forEach((formEl) => {
+    formEl.addEventListener("submit", (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formEl, options);
+
+    // look for all inputs in the form
+    // loop through all inputs to validate
+    // if input is invalid:
+    // get validation message
+    // add error class to input
+    // display error message
+    // disable button
+
+    // if all inputs valid:
+    // enable button
+    // reset error messages
+  });
+}
+
+const config = {
+  formSelector: ".modal__form",
+  inputSelector: ".modal__input",
+  submitButtonSelector: ".modal__button",
+  inactiveButtonClass: "modal__button_disabled",
+  inputErrorClass: "modal__input_type_error",
+  errorClass: "modal__error_visible",
+};
+
+enableValidation(config);
