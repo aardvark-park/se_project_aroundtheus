dc04c6b372e336eb80586e02360fdcdea6860fd5
Brandon Kincaid <kincaid.brandon@live.com>
2026-09-08 17:52:13 -0700

    update todo

--- src/pages/index.js
+++ src/pages/index.js
@@ -121,4 +121,4 @@ Constants.theme.addEventListener("click", () => {
 // fix: _getInputValues in PopupWithForm.js
 // set up Section.js
 // set up UserInfo.js
-// fix: if user has browser set to prefer light mode, toggle does not function
+// fix: ghpages, currently just raw HTML
